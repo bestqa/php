7edb52a733b531fb1edea6619386c012829de166
chxd99 <chxd99@gmail.com>
2013-04-15 09:33:05 -0600

    changed articles.php to articles_show.php

--- banner.php
+++ banner.php
@@ -1,6 +1,6 @@
 <div id="banner">
 	<div id="logo">
-		<a href="index.html">
+		<a href="index.php">
 			<img src="images/logo.gif"/>
 		</a>
 	</div>
@@ -11,11 +11,11 @@
 			<a href="http://forum.bestqa.org/wp-login.php" target="_blank">Sign In</a>
 		</div>
 		<div id="menu_bar">
-			<a href="index.html" class="menu_bar">Home</a> &nbsp;&nbsp;
-			<a href="index.html" class="menu_bar">Mission</a> &nbsp;&nbsp;
-			<a href="index.html" class="menu_bar">Articles</a> &nbsp;&nbsp;
-			<a href="index.html" class="menu_bar">Seminars</a> &nbsp;&nbsp;
-			<a href="index.html" class="menu_bar">Resources</a> &nbsp;&nbsp;
+			<a href="index.php" class="menu_bar">Home</a> &nbsp;&nbsp;
+			<a href="index.php" class="menu_bar">Mission</a> &nbsp;&nbsp;
+			<a href="articles_show.php" class="menu_bar">Articles</a> &nbsp;&nbsp;
+			<a href="index.php" class="menu_bar">Seminars</a> &nbsp;&nbsp;
+			<a href="index.php" class="menu_bar">Resources</a> &nbsp;&nbsp;
 			<a href="http://forum.bestqa.org" class="menu_bar" style="text-decoration:none;color:white;background:#000000;border-radius:5px 5px 5px;" target='_blank' title="Goto WordPress Site">Forum</a>
 		</div>
 	</div>
