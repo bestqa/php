b5c241038d844631fa6535e5ea44afef40cc4bf6
Xiaodong Chen <javen@javen-laptop.(none)>
2013-07-06 01:24:25 +0800

    del the link to heroku:

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<body class="body_width" onselectstart="return false">
 		<?php include 'banner.php'; ?>
 		<div style="margin:0;padding:0;">
-			<h1>The website is <a href="http://bestqa.herokuapp.com/" style="text-decoration:none;">building ...</a>, plz follow us on Social Medias<h1>
+			<h1>The website is <a href="https://github.com/bestqa/php" style="text-decoration:none;">building ...</a>, plz follow us on Social Medias<h1>
 		</div>
 		<?php include 'footer.php'; ?>
 	</body>
