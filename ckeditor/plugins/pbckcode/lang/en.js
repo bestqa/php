f00853cd8745b79fae85747712f50d445b51ffa4
Xiaodong Chen <javen@javen-laptop.(none)>
2013-07-13 11:36:17 +0800

    Add pbckcode plugin to ckeditor for highlight languages.

---
+++ ckeditor/plugins/pbckcode/lang/en.js
@@ -0,0 +1,4 @@
+CKEDITOR.plugins.setLang("pbckcode","en",
+{
+	title: 'Add code',
+});
---
+++ ckeditor/plugins/pbckcode/plugin.js
@@ -0,0 +1,21 @@
+CKEDITOR.plugins.add('pbckcode', {
+	icons: 'pbckcode',
+	lang : 'en', 
+	init: function(editor) {
+		// load JS file
+		var head = document.getElementsByTagName('HEAD').item(0);
+		var script= document.createElement("script");
+		script.type = "text/javascript";
+		script.src = CKEDITOR.plugins.getPath('pbckcode') + "dialogs/ace/ace.js";
+		head.appendChild(script);
+	
+		editor.addCommand('pbckcodeCommand', new CKEDITOR.dialogCommand('pbckcodeDialog'));	
+	
+		editor.ui.addButton('pbckcode', {
+			label: editor.lang.pbckcode.title,
+			command: 'pbckcodeCommand'
+		});
+
+		CKEDITOR.dialog.add('pbckcodeDialog', this.path + 'dialogs/pbckcode.js' );
+	}	
+});
