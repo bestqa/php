6b445d2aa79aac86d8307ae754d5a6c3fa8fd5f6
Xiaodong Chen <chxd99@gmail.com>
2015-05-01 19:59:22 +0800

    Added contents to home page

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <body class="body_width" onselectstart="return false">
         <?php include 'banner.php'; ?>
         <div style="margin:0;padding:0;">
-            <h1>The website is <a href="https://github.com/bestqa/php" style="text-decoration:none;">building ...</a>, plz follow us on Social Medias<h1>
+            <h3 align='left'>Welcome to the BestQA orgnization.</h3>
+            <p> Our website aims to gather all best Quality Assurance knowledge and practices in the software world. We're not only focusing on software testing technology, we also discuss QA process, including agile and continuous integration. </p>
+            <p> We believe that a good QA orgnization can help a software company build better quality softwares and make the company more success.</p>
+            <p>The website is <a href="https://github.com/bestqa/php" style="text-decoration:none;">building ...</a>, plz follow us on Social Medias</p>
         </div>
         <?php include 'footer.php'; ?>
      </body>
