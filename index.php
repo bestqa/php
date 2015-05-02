54ebbc42ba7574afc98f2799e7f391bca50f8b0a
Xiaodong Chen <chxd99@gmail.com>
2015-05-02 21:22:26 +0800

    Modified a typo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <div style="margin:0;padding:0;">
             <h3 align='left'>Welcome to the BestQA orgnization.</h3>
             <p> Our website aims to gather all best Quality Assurance knowledge and practices in the software world. We're not only focusing on software testing technology, we also discuss QA process, including agile and continuous integration. </p>
-            <p> We believe that a good QA orgnization can help a software company build better quality softwares and make the company more success.</p>
+            <p> We believe that a good QA orgnization can help a software company build better quality softwares and make the company more successful.</p>
             <p> The website is <a href="https://github.com/bestqa/php" style="text-decoration:none;">building ...</a>, plz follow us on Social Medias</p>
         </div>
         <?php include 'footer.php'; ?>
