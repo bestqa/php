d81757f7c2653ff6a3d2d1457f8076884de656d9
Xiaodong Chen <chxd99@gmail.com>
2015-05-02 23:04:48 +0800

    added paragraph

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
         <div style="margin:0;padding:0;">
             <h3 align='left'>Welcome to the BestQA orgnization.</h3>
             <p> Our website aims to gather all best Quality Assurance knowledge and practices in the software world. We're not only focusing on software testing technology, we also discuss QA process, including agile and continuous integration. </p>
-            <p> We believe that a good QA orgnization can help a software company build better quality softwares and make the company more successful.</p>
+            <p> We never thought that QA is just testing engineer, everyone who wants to make a great software would be a QA, you might be a developer, a tester, a product designer, or a senior manager and even a sales one in software company. </p>
+            <p> We always believe that an excellent QA team can help a software company build better quality products and make the company more successful.</p>
             <p> The website is <a href="https://github.com/bestqa/php" style="text-decoration:none;">building ...</a>, plz follow us on Social Medias</p>
         </div>
         <?php include 'footer.php'; ?>
