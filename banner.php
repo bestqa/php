83f94f5216fb041a6479cbe0018c5ec0009281da
Xiaodong Chen <chxd99@gmail.com>
2015-06-22 00:23:51 +0800

    Disabled Sign Up, Sign In, Seminar and Resource link in home page.

--- banner.php
+++ banner.php
@@ -7,14 +7,14 @@
 	<div id="log_tail">.org</div>
 	<div id="navigator">
 		<div id="sign_in_up"> 
-			<a href="http://forum.bestqa.org/wp-register.php" target="_blank">Sign Up</a> &nbsp;
-			<a href="http://forum.bestqa.org/wp-login.php" target="_blank">Sign In</a>
+			<a href="http://forum.bestqa.org/wp-register.php" target="_blank" class="not-active">Sign Up</a> &nbsp;
+			<a href="http://forum.bestqa.org/wp-login.php" target="_blank" class="not-active">Sign In</a>
 		</div>
 		<div id="menu_bar">
 			<a href="index.php" class="menu_bar">Mission</a> &nbsp;&nbsp;
 			<a href="articles_show.php" class="menu_bar">Articles</a> &nbsp;&nbsp;
-			<a href="index.php" class="menu_bar">Seminars</a> &nbsp;&nbsp;
-			<a href="index.php" class="menu_bar">Resources</a> &nbsp;&nbsp;
+			<a href="index.php" class="not-active">Seminars</a> &nbsp;&nbsp;
+			<a href="index.php" class="not-active">Resources</a> &nbsp;&nbsp;
 			<a href="bug_tracker.php" class="menu_bar">Mantis</a> &nbsp;&nbsp;
 			<a href="http://forum.bestqa.org" class="menu_bar" style="text-decoration:none;color:white;background:#000000;border-radius:5px 5px 5px;" target='_blank' title="Goto WordPress Site">Forum</a>
 		</div>
